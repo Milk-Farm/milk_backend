e3db5e51ab31cbdade63d22890f68ca0ff7a7780
Vikas Joshi <vikasjoshis001@gmail.com>
2023-08-20 13:08:45 +0530

    fifth commit

--- milkfarm_backend/settings.py
+++ milkfarm_backend/settings.py
@@ -80,12 +80,24 @@ WSGI_APPLICATION = 'milkfarm_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'NAME': str(BASE_DIR / 'db.sqlite3'),
+
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': 'postgresql://postgres:lCAXw3X26dJvk4AbEaj7@containers-us-west-110.railway.app:7538/railway',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'lCAXw3X26dJvk4AbEaj7',
+        'HOST': 'containers-us-west-110.railway.app',
+        'PORT': 7538,
     }
 }
 
